2eb17e4fb51490c51f4d60a63e61d80ad3c002fa
linuxlivre <82552841+tiaoedi@users.noreply.github.com>
2022-10-14 16:39:48 -0300

    Add files via upload

--- apps-core/kernel-linux-xanmod-edge.sh
+++ apps-core/kernel-linux-xanmod-edge.sh
@@ -19,40 +19,8 @@ echo "################################################################"
 tput sgr0
 echo
 
-
-wget https://aur.archlinux.org/cgit/aur.git/snapshot/linux-xanmod-edge.tar.gz
-
-tar xzf linux-xanmod-edge*
-
-rm linux-xanmod-edge.tar.gz
-
-FIND="_microarchitecture=0"
-REPLACE="_microarchitecture=38"
-sed -i "s/$FIND/$REPLACE/g" linux-xanmod-edge/PKGBUILD
-
-# FIND="use_numa=y"
-# REPLACE="use_numa=n"
-# sed -i "s/$FIND/$REPLACE/g" linux-xanmod/PKGBUILD
-
-# FIND="use_tracers=y"
-# REPLACE="use_tracers=n"
-# sed -i "s/$FIND/$REPLACE/g" linux-xanmod/PKGBUILD
-
-# FIND="_compiler=gcc"
-# REPLACE="_compiler=gcc"
-# sed -i "s/$FIND/$REPLACE/g" linux-xanmod/PKGBUILD
-
-# FIND="_compress_modules=n"
-# REPLACE="_compress_modules=n"
-# sed -i "s/$FIND/$REPLACE/g" linux-xanmod/PKGBUILD
-
-FIND="_localmodcfg=n"
-REPLACE="_localmodcfg=y"
-sed -i "s/$FIND/$REPLACE/g" linux-xanmod-edge/PKGBUILD
-
-cd linux-xanmod-edge
-
-makepkg -s --skippgpcheck
+sudo pacman -S --noconfirm --needed linux-xanmod-edge 
+sudo pacman -S --noconfirm --needed linux-xanmod-edge-headers
 
 
 echo
