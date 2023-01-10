ee326ccecd4258419cb62704bce1d2931cac78ef
linuxlivre <82552841+tiaoedi@users.noreply.github.com>
2023-01-10 12:06:15 -0300

    Add files via upload

--- archlinux conf/dwm-2023/config.def.h
+++ archlinux conf/dwm-2023/config.def.h
@@ -13,9 +13,9 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = {"Nerd Fonts:size=8"};
+static const char *fonts[]          = {"JetBrainsMono Nerd Font:size=10"};
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#220854";
+static const char col_gray1[]       = "#1c0644";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
