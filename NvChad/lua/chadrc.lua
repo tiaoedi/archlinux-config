283d6c3039790827ed5443e1d9e64a8c61d83cf2
linuxlivre <82552841+tiaoedi@users.noreply.github.com>
2023-02-03 22:57:26 -0300

    Add files via upload

--- NvChad/lua/chadrc.lua
+++ NvChad/lua/chadrc.lua
@@ -1,7 +1,7 @@
 local M = {
    ui = {
-      theme = "onedark",
-      fav_themes = { "onedark", "gruvchad" }, -- for theme toggle
+      theme = "chadracula",
+      fav_themes = { "chadracula", "gruvchad" }, -- for theme toggle
       theme_toggler = false,
       hidden_statusline = {
          -- these are filetypes, not pattern matched
