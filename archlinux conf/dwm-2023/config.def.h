bc48e86fa3e6b860c51d4b3e690a070f814c502a
linuxlivre <82552841+tiaoedi@users.noreply.github.com>
2023-03-17 13:50:48 -0300

    Add files via upload

--- archlinux conf/dwm-2023/config.def.h
+++ archlinux conf/dwm-2023/config.def.h
@@ -15,11 +15,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {"JetBrainsMono Nerd Font:size=10" ,"JetBrainsMono Nerd Font Mono:style:medium:size=12","Material Design Icons Desktop:size=11"};
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#0B021B";
+static const char col_gray1[]       = "#282a36";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray3[]       = "#eeeeee";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#504477";
+static const char col_cyan[]        = "#ff79c6";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -27,8 +27,8 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static char *tags[] = { "-Arch-", "" , "", "", ""};
-//static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static char *tags[] = { "-rch-", "" , "", "", ""};
+//static const char *tags[] = { "-Arch-", "" , "", "", "" };
 //static char *tags[] = { "1️⃣", "2️⃣" , "3️⃣", "4️⃣", "5️⃣" , "6️⃣", "7️⃣", "8️⃣", "9️⃣ " };
 //static char *tags[] = { "✔️", "🔥" , "📩", "⚙️", "🏠" , "📽️", "🖥️", "📂", "📻 " };
 
@@ -43,7 +43,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Gimp",     NULL,       NULL,       0,            0,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
@@ -58,6 +58,7 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "><>",      monocle }, /* new entry for floating layout */
 };
 
 /* key definitions */
@@ -136,4 +137,5 @@ static const Button buttons[] = {
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+ 
 };
