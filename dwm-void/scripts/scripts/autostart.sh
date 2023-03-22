c7a3f6e3ef1d9df1da701ae331bc4b52c876f323
linuxlivre <82552841+tiaoedi@users.noreply.github.com>
2023-03-22 13:19:22 -0300

    Add files via upload

--- dwm-void/scripts/scripts/autostart.sh
+++ dwm-void/scripts/scripts/autostart.sh
@@ -1,29 +1,40 @@
 #!/bin/bash
 
-setxkbmap -layout br
-xbacklight -set 25 &
-xset r rate 300 50 &
-xset s off &
-xset -dpms &
-pplet &
+pplet
 run pamac-tray
 #run "variety"
 xfce4-power-manager &
 #run blueberry-tray
-/usr/libexec/xfce-polkit &
+#xrandr -s 1920x1080 &
+xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 & 
+/usr/lib/xfce4/notifyd/xfce4-notifyd &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 picom -b  --config ~/.config/dwm/picom.conf &
 numlockx on
 #volumeicon &
 sxhkd -c ~/.config/dwm/sxhkdrc &
 nitrogen --restore
-
 ~/.config/dwm/slstatus/slstatus &
 xset led 3 &
 kdeconnect-indicator &
-dropbox &
+xset r rate 300 50 &
+xbacklight -set 35 &
+xset s off &
+xset -dpms &
+albert &
+##dropbox &
 ##run "insync start"
 ##run "spotify"
 ##run "ckb-next -b"
 ##run "discord"
 ##run "telegram-desktop"
 ##loop
+
+
+
+
+
+
+
+
+
