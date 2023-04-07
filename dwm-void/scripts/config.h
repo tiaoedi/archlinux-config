d89e4085e48af750e8a0029bf5e3f01721289f6d
linuxlivre <82552841+tiaoedi@users.noreply.github.com>
2023-04-07 12:53:26 -0300

    Add files via upload

--- dwm-void/scripts/config.h
+++ dwm-void/scripts/config.h
@@ -13,13 +13,13 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = {"JetBrainsMono Nerd Font:size=10" ,"JetBrainsMono Nerd Font Mono:style:medium:size=12","Material Design Icons Desktop:size=11"};
+static const char *fonts[]          = {"JetBrainsMonoMedium Nerd Font:size=10" ,"JetBrainsMonoMedium Nerd Font:size=10","JetBrainsMonoMedium Nerd Font:size=10"};
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#0B021B";
+static const char col_gray1[]       = "#282a36";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#ff79c6";
+static const char col_cyan[]        = "#2d3146";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
