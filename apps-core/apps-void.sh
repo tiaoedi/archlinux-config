436fdf80a6d9208b659fbcd060fb1855e639bea4
linuxlivre <82552841+tiaoedi@users.noreply.github.com>
2023-04-11 00:18:33 -0300

    Add files via upload

--- apps-core/apps-void.sh
+++ apps-core/apps-void.sh
@@ -4,6 +4,7 @@ sudo xbps-install -Syu
 #sudo xbps-install -S void-repo-multilib void-repo-nonfree void-repo-multilib-nonfree
 sudo xbps-install -Syu
 
+sudo xbps-install -Syu libX11 libXft pkg-config
 sudo xbps-install -Syu git make gcc                      
 sudo xbps-install -Syu dmenu
 sudo xbps-install -Syu sxhkd
@@ -45,6 +46,7 @@ sudo xbps-install -Syu leafpad go
 sudo xbps-install -Syu font-inconsolata-otf
 sudo xbps-install -Syu flatpak
 sudo xbps-install -Syu firefox feh expat
+sudo xbps-install -Syu kdeconnect-22.12.1_2
 sudo xbps-install -Syu dunst dejavu-fonts-ttf dbus
 sudo xbps-install -Syu curl cargo btop wget
 sudo xbps-install -Syu alsa-tools alsa-utils
@@ -74,6 +76,11 @@ sudo xbps-install -Syu qt5ct
 sudo xbps-install -Syu kvantum-1.0.5_1  
 sudo xbps-install -Syu qbittorrent-4.5.2_1
 sudo xbps-install -Syu lolcat-c-1.4_1
+sudo xbps-install -Syu guvcview
+sudo xbps-install -Syu kdenlive
+sudo xbps-install -Syu obs
+
+
 
 sudo xbps-install -Syu xorg xorg-server xorg-apps 
 #sudo xbps-install -S lightdm lightdm-gtk-greeter
