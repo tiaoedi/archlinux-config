66705366ba9a02719741a90cbc08dafd8fb799fe
linuxlivre <82552841+tiaoedi@users.noreply.github.com>
2023-05-14 19:01:27 -0300

    Add files via upload

--- archlinux conf/dwm-2023/scripts/autostart.sh
+++ archlinux conf/dwm-2023/scripts/autostart.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-xbacklight -set 35 &
-xset r rate 300 50 &
-xset s off &
-xset -dpms &
-#xrandr -s 1920x1080 &
-xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 & 
 
+if [ -d /etc/X11/xinit/xinitrc.d ]; then
+  for f in /etc/X11/xinit/xinitrc.d/*; do
+    [ -x "$f" ] && . "$f"
+  done
+  unset f
+fi
 
 
 pplet
@@ -21,12 +21,19 @@ numlockx on
 #volumeicon &
 sxhkd -c ~/.config/dwm/sxhkdrc &
 nitrogen --restore
-albert &
-~/.config/dwm/slstatus/slstatus &
+
+slstatus &
+xbacklight -set 35 &
+xrandr --output HDMI2 --mode 1920x1080 &
 xset led 3 &
 kdeconnect-indicator &
 xset r rate 300 50 &
 xset s off &
 xset -dpms &
-##dropbox &
-
+dropbox &
+##run "insync start"
+##run "spotify"
+##run "ckb-next -b"
+##run "discord"
+##run "telegram-desktop"
+##loop
