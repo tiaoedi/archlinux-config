c7c3785d827aa81f1cef135dc184f153f9d6a69f
linuxlivre <82552841+tiaoedi@users.noreply.github.com>
2023-06-14 22:01:27 -0300

    Add files via upload

--- archlinux conf/dwm-2023/config.def.h
+++ archlinux conf/dwm-2023/config.def.h
@@ -56,8 +56,8 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ " ",      tile },    /* first entry is default */
+	{ " ",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 	{ "><>",      monocle }, /* new entry for floating layout */
 };
