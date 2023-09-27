5dbaf8ac5559a54d9d5b3562a596499035b3cbc5
linuxlivre <82552841+tiaoedi@users.noreply.github.com>
2023-09-27 18:52:30 -0300

    Add files via upload

--- apps-core/apps-void.sh
+++ apps-core/apps-void.sh
@@ -18,7 +18,7 @@ sudo xbps-install -Syu ConsoleKit2
 sudo xbps-install -Syu elogind
 sudo xbps-install -Syu udisks2
 sudo xbps-install -Syu cgmanager 
-sudo xbps-install -Syu zsh zathura-pdf-mupdf
+#sudo xbps-install -Syu zsh zathura-pdf-mupdf
 sudo xbps-install -Syu xtools xrandr xprop
 sudo xbps-install -Syu unzip
 sudo xbps-install -Syu tar
@@ -27,7 +27,7 @@ sudo xbps-install -Syu qalculate
 sudo xbps-install -Syu picom
 sudo xbps-install -Syu pavucontrol
 sudo xbps-install -Syu numlockx
-sudo xbps-install -Syu nitrogen
+#sudo xbps-install -Syu nitrogen
 sudo xbps-install -Syu neofetch
 sudo xbps-install -Syu mpv 
 sudo xbps-install -Syu lxappearance 
@@ -39,13 +39,13 @@ sudo xbps-install -Syu dunst dbus
 sudo xbps-install -Syu curl wget
 sudo xbps-install -Syu alacritty 
 sudo xbps-install -Syu pipewire
-sudo xbps-install -Syu xmirror
+#sudo xbps-install -Syu xmirror
 sudo xbps-install -Syu ufw
-sudo xbps-install -Syu dropbox
+#sudo xbps-install -Syu dropbox
 sudo xbps-install -Syu tumbler-4.18.0_1
 sudo xbps-install -Syu Thunar-4.18.4_1 
 sudo xbps-install -Syu xfce4-notifyd-0.6.4_1
-sudo xbps-install -Syu thunderbird
+#sudo xbps-install -Syu thunderbird
 sudo xbps-install -Syu ristretto
 sudo xbps-install -Syu xdg-user-dirs-0.18_1
 sudo xbps-install -Syu mtools-4.0.42_1 
@@ -55,21 +55,21 @@ sudo xbps-install -Syu sublime-text4-4143_1
 sudo xbps-install -Syu gnome-disk-utility-44.0_1
 sudo xbps-install -Syu cmatrix
 sudo xbps-install -Syu python3-requests-2.28.2_1 
-sudo xbps-install -Syu chromium-111.0.5563.64_1
+#sudo xbps-install -Syu chromium-111.0.5563.64_1
 sudo xbps-install -Syu qt5ct
 sudo xbps-install -Syu kvantum-1.0.5_1  
-sudo xbps-install -Syu qbittorrent-4.5.2_1
+#sudo xbps-install -Syu qbittorrent-4.5.2_1
 sudo xbps-install -Syu lolcat-c-1.4_1
-sudo xbps-install -Syu guvcview
-sudo xbps-install -Syu kdenlive
-sudo xbps-install -Syu obs
+#sudo xbps-install -Syu guvcview
+#sudo xbps-install -Syu kdenlive
+#sudo xbps-install -Syu obs
 sudo xbps-install -Syu arc-theme-20221218_1
 
-sudo xbps-install -Sy nix
-sudo ln -s /etc/sv/nix-daemon /var/service
-source /etc/profile
-nix-channel --add http://nixos.org/channels/nixpkgs-unstable
-nix-channel --update
+#sudo xbps-install -Sy nix
+#sudo ln -s /etc/sv/nix-daemon /var/service
+#source /etc/profile
+#nix-channel --add http://nixos.org/channels/nixpkgs-unstable
+#nix-channel --update
 
 sh inst-fonts.sh*
 
@@ -82,15 +82,15 @@ sh inst-fonts.sh*
 
 
 
-#sudo ln -s /etc/sv/dbus /var/service
-#sudo ln -s /etc/sv/cgmanager /var/service/
-#sudo ln -s /etc/sv/consolekit /var/service/
-#sudo ln -s /etc/sv/polkitd /var/service/
+sudo ln -s /etc/sv/dbus /var/service
+sudo ln -s /etc/sv/cgmanager /var/service/
+sudo ln -s /etc/sv/consolekit /var/service/
+sudo ln -s /etc/sv/polkitd /var/service/
 #sudo ln -s /etc/sv/lightdm /var/service/
 #sudo ln -s /etc/sv/sddm /var/service/
-#sudo ln -s /etc/sv/pipewire /var/service/
-#sudo ln -s /etc/sv/pipewire-pulse /var/service/
-#sudo ln -s /etc/sv/ufw /var/service/
+sudo ln -s /etc/sv/pipewire /var/service/
+sudo ln -s /etc/sv/pipewire-pulse /var/service/
+sudo ln -s /etc/sv/ufw /var/service/
 #sudo ln -s /etc/sv/dhcpcd /var/service
 #sudo ln -s /etc/sv/dhcpcd-eth0 /var/service
 #sudo ln -s /etc/sv/wpa_supplicant /var/service
