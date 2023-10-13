1b573dc3dad113ecb81251f474438d46d5164342
linuxlivre <82552841+tiaoedi@users.noreply.github.com>
2023-10-12 23:31:57 -0300

    Add files via upload

--- apps-core/dwm-apps-core.sh
+++ apps-core/dwm-apps-core.sh
@@ -285,7 +285,9 @@ sudo cp Xsetup /usr/share/sddm/scripts/
 sudo cp sddm.conf /etc/
 
 sudo mkdir -p /etc/X11/xorg.conf.d
-sudo cp /etc/X11/xorg.conf.d/20-intel.conf
+sudo cp 20-intel.conf /etc/X11/xorg.conf.d
+sudo cp environment /etc/
+
 
 
 #bash -c "$(wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)"
